96bef56a7b4cdedef810ea1faa39b51daca3d86c
Leandro Vásquez Vega <laen03@estudiantec.cr>
2025-04-29 10:19:06 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,320 @@
+const express = require("express");
+const admin = require("./firebase-admin");
+const cors = require("cors");
+const app = express();
+const multer = require("multer");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Initialize firebase admin
+const PORT = process.env.PORT || 3000;
+const db = admin.firestore();
+const bucket = admin
+  .storage()
+  .bucket("gs://prostem-db-68733.firebasestorage.app");
+
+//File middleware
+const upload = multer({ storage: multer.memoryStorage() });
+
+function getFullNameFromToken(decodedTokenName) {
+  const parts = decodedTokenName.trim().split(/\s+/);
+  let name = "",
+    lastName1 = "",
+    lastName2 = "";
+
+  if (parts.length === 1) {
+    name = parts[0];
+  } else if (parts.length === 2) {
+    [name, lastName1] = parts;
+  } else if (parts.length === 3) {
+    [name, lastName1, lastName2] = parts;
+  } else {
+    // Assume the name is everything except the last two parts of the string
+    name = parts.slice(0, -2).join(" ");
+    lastName1 = parts[parts.length - 2];
+    lastName2 = parts[parts.length - 1];
+  }
+
+  return { name, lastName1, lastName2 };
+}
+
+//SIGN UP WITH EMAIL & PASSWORD
+app.post(
+  "/api/signUp-emailPassword",
+  upload.single("photo"),
+  async (request, response) => {
+    const {
+      email,
+      password,
+      name,
+      lastName1,
+      lastName2,
+      phone,
+      birthDate,
+      institution,
+      interests,
+    } = request.body;
+
+    try {
+      //Create user in Firebase Authentication
+      const userRecord = await admin.auth().createUser({
+        email,
+        password,
+        displayName: `${name} ${lastName1}`,
+      });
+
+      let photoURL = null;
+      if (request.file) {
+        const fileName = `profilePictures/${userRecord.uid}`;
+        const file = bucket.file(fileName);
+
+        await file.save(request.file.buffer, {
+          metadata: {
+            contentType: request.file.mimetype,
+          },
+        });
+        // This makes the file public
+        //TODO: use signed tokens if privacy is needed
+        await file.makePublic();
+        photoURL = file.publicUrl();
+      }
+      const parsedInterests = JSON.parse(interests || "[]");
+      await db.collection("users").doc(userRecord.uid).set({
+        email,
+        name,
+        lastName1,
+        lastName2,
+        phone,
+        birthDate,
+        institution,
+        interests: parsedInterests,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        active: true,
+        myEvents: null,
+        role: "user",
+        photoURL,
+      });
+      response
+        .status(201)
+        .json({ message: "Usuario registrado", uid: userRecord.uid });
+    } catch (error) {
+      console.error("Error adding user:", error);
+      return response.status(400).json({ error: error.message });
+      //return response.status(400).send({ error: error.message });
+    }
+  }
+);
+
+//SIGN UP WITH GOOGLE
+app.post(
+  "/api/signUp-Google",
+  upload.single("photo"),
+  async (request, response) => {
+    const { idToken, phone, birthDate, institution, interests } = request.body;
+
+    try {
+      // 1. Verify the idToken with Firebase
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      const uid = decodedToken.uid;
+      const email = decodedToken.email;
+      const googlePhoto = decodedToken.picture;
+      //Try to get the name and surnames
+      const { name, lastName1, lastName2 } = getFullNameFromToken(
+        decodedToken.name
+      );
+
+      let photoURL = googlePhoto || null;
+
+      //Upload image to storage if the user uploaded their own.
+      if (request.file) {
+        const fileName = `profilePictures/${uid}`;
+        const file = bucket.file(fileName);
+
+        await file.save(request.file.buffer, {
+          metadata: { contentType: request.file.mimetype },
+        });
+
+        await file.makePublic();
+        photoURL = file.publicUrl();
+      }
+
+      //Save data to Firestore
+      const parsedInterests = JSON.parse(interests || "[]");
+      await db.collection("users").doc(uid).set({
+        email,
+        name,
+        lastName1,
+        lastName2,
+        phone,
+        birthDate,
+        institution,
+        interests: parsedInterests,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        active: true,
+        myEvents: null,
+        role: "user",
+        photoURL,
+      });
+
+      response
+        .status(201)
+        .json({ message: "Usuario registrado con Google", uid });
+    } catch (error) {
+      console.error("Error al registrar con Google:", error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+);
+
+//SIGN IN WITH EMAIL & PASSWORD
+app.post("/api/signIn-emailPassword", async (request, response) => {
+  const { idToken } = request.body;
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+
+    // Search the user's data on Firestore
+    const userDoc = await admin.firestore().collection("users").doc(uid).get();
+
+    if (!userDoc.exists) {
+      return response
+        .status(404)
+        .json({ message: "Usuario no registrado en la base de datos" });
+    }
+
+    const userData = userDoc.data();
+    response.status(200).json({ message: "Login exitoso", user: userData });
+  } catch (error) {
+    console.error("Error verificando token:", error);
+    response
+      .status(401)
+      .json({ message: "Token inválido", error: error.message });
+  }
+});
+
+//SIGN IN WITH GOOGLE
+app.post("/api/signIn-Google", async (request, response) => {
+  const { idToken } = request.body;
+
+  if (!idToken) {
+    console.log("No token");
+    return res.status(400).json({ message: "Token no proporcionado" });
+  }
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+
+    // Search the user's data on Firestore
+    const userDoc = await admin.firestore().collection("users").doc(uid).get();
+
+    if (!userDoc.exists) {
+      await admin.auth().deleteUser(uid);
+      return response
+        .status(403)
+        .json({ message: "Usuario no registrado en la base de datos" });
+    }
+
+    const userData = userDoc.data();
+    return response
+      .status(200)
+      .json({ message: "Login exitoso", user: userData });
+  } catch (error) {
+    console.error("Error verificando token:", error);
+    response
+      .status(401)
+      .json({ message: "Token inválido", error: error.message });
+  }
+});
+
+//CREATE AN EVENT
+app.post("/api/create-event", async (request, response) => {
+  try {
+    const {
+      capacity,
+      date,
+      description,
+      endTime,
+      place,
+      startTime,
+      title,
+      virtualEvent,
+    } = request.body;
+
+    const eventData = {
+      attendees: {},
+      capacity: Number(capacity),
+      date,
+      description,
+      endTime,
+      place,
+      registeredUsers: [],
+      startTime,
+      survey: null,
+      title,
+      virtualEvent: Boolean(virtualEvent),
+    };
+
+    const docRef = await db.collection("events").add(eventData);
+    response.status(201).json({ message: "Evento creado", id: docRef.id });
+  } catch (error) {
+    console.error("Error al crear evento:", error);
+    response.status(500).json({ message: "Error del servidor" });
+  }
+});
+
+//GET ALL EVENTS
+app.get("/api/get-events", async (request, response) => {
+  try {
+    const eventsSnapshot = await db.collection("events").get();
+    const events = eventsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    response.status(200).json(events);
+  } catch (error) {
+    console.error("Error al obtener eventos:", error);
+    response.status(500).json({ message: "Error al obtener eventos" });
+  }
+});
+
+//#################################################################################################
+
+// // DELETE USER
+// app.delete("/api/delete-user/:id", async (request, response) => {
+//   try {
+//     const userID = request.params.id;
+
+//     const userDocRef = db.collection("users").doc(userID);
+//     await userDocRef.delete();
+
+//     return response.status(200).send();
+//   } catch (error) {
+//     console.log(error);
+//     //return response.status(500).json({ error: error.message });
+//     return response.status(500).send(error);
+//   }
+// });
+
+app.listen(PORT, (error) => {
+  if (error) console.log("There was an error:", error);
+  console.log(`App available on http://localhost:${PORT} `);
+});
+
+// const role = "admin";
+// const data = {
+//   active: true,
+//   birthDate: "1999-10-30",
+//   email: "mtrevino@tec.ac.cr",
+//   insitutution: "ITCR",
+//   insterests: [],
+//   lastname1: "Treviño",
+//   lastname2: "Villalobos",
+//   myEvents: [],
+//   name: "Marlen",
+//   password: "123456",
+//   phone: "",
+//   photo: "",
+//   role: role,
+//   uid: "",
+// };
